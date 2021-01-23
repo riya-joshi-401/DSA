d93106fb4f6db4fced81bb8dabdd5fb8d1b19182
RIYA JOSHI <riya.joshi@somaiya.edu>
2021-01-24 00:00:10 +0530

    Create 2D_array.cpp

---
+++ Arrays/2D_array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+// a two-dimensional m ¥ n array is an array that contains m ¥ n data elements and each element is accessed using two subscripts, i and j, where i<=m and j<=n
+
+int marks[3][5];
+
+/*
+
+          Col 0         Col 1        Col 2       Col 3       Col 4
+ Row 0    marks[ ][ ] marks[ ][1] marks[ ][2] marks[ ][3] marks[ ][4]
+ Row 1    marks[1][ ] marks[1][1] marks[1][2] marks[1][3] marks[1][4]
+ Row 2    marks[2][ ] marks[2][1] marks[2][2] marks[2][3] marks[2][4]
+
+*/
+
+
+return 0;
+
+}
