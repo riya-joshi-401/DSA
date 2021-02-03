f901b7b811cb85e255c27fe897a8098e5ceed2da
RIYA JOSHI <riya.joshi@somaiya.edu>
2021-02-03 13:44:40 +0530

    Create kandane.py

---
+++ kandane.py
@@ -0,0 +1,21 @@
+
+def kandane(A):
+    
+    n=len(A)
+    local_max=0
+    global_max=float('-inf')
+    
+    for i in range(n):
+        
+        local_max=max(A[i],A[i]+local_max)
+        
+        if local_max>global_max:
+            global_max=local_max
+            
+    return global_max
+    
+
+    
+A=[-2, 1, -3, 4]
+ans=kandane(A)
+print("Maximum sum of subarray : ",ans)
