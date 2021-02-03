a68f98f88bdd02d35669d8a6e9c4bde789e95280
RIYA JOSHI <riya.joshi@somaiya.edu>
2021-02-03 14:54:31 +0530

    Update binary_search.py

--- searching/binary_search.py
+++ searching/binary_search.py
@@ -1,25 +1,25 @@
 
 def binarysearch(self, arr, n, k):
-		
+	
 	l=0
 	r=n-1
 		
-    while l <= r: 
+	while l <= r: 
   
-        mid = l + (r - l) // 2
-              
-        # Check if x is present at mid 
-        if arr[mid] == k: 
-            return mid 
-      
-        # If x is greater, ignore left half 
-        elif arr[mid] < k: 
-            l = mid + 1
-      
-        # If x is smaller, ignore right half 
-        else: 
-            r = mid - 1
-      
-    # if we reach here, then the element was not present 
-    return -1
+		mid = l + (r - l) // 2
+
+		# Check if x is present at mid 
+		if arr[mid] == k: 
+		    return mid 
+
+		# If x is greater, ignore left half 
+		elif arr[mid] < k: 
+		    l = mid + 1
+
+		# If x is smaller, ignore right half 
+		else: 
+		    r = mid - 1
+		
+	# if we reach here, then the element was not present 
+	return -1
 
