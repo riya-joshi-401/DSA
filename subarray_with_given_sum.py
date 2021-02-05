04568ca68092d44afa62fe3b84cca4601c560634
RIYA JOSHI <riya.joshi@somaiya.edu>
2021-02-05 13:06:33 +0530

    Create subarray_with_given_sum.py

---
+++ subarray_with_given_sum.py
@@ -0,0 +1,14 @@
+def subArraySum(A, n, x): 
+    
+    for i in range(n):
+      temp=0
+      j=i
+      
+      while (temp <x) and (j<n):
+          temp=temp+A[j]
+          if temp==x:
+              return [i+1,j+1]
+              
+          j+=1
+    return [-1]
+    
