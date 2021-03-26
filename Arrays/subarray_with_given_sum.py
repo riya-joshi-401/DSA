f9d622f3ed626aa54a9c6060df5946ef3ceafbc4
RIYA JOSHI <riya.joshi@somaiya.edu>
2021-03-26 20:12:48 +0530

    Update subarray_with_given_sum.py

--- Arrays/subarray_with_given_sum.py
+++ Arrays/subarray_with_given_sum.py
@@ -23,6 +23,9 @@ def subArrayExists(arr,n):
         sum += arr[i]
         print("sum",sum)
         
+        #if prefix sum is 0 or if it is already present in set then it is
+    		#repeated which means there is a subarray whose summation was 0,
+    		#so we return true.
         if sum == 0 or sum in s:
             return True
             break
