de8d3350d5ad7eb23bc3aa4d4b4240155dad9908
RIYA JOSHI <riya.joshi@somaiya.edu>
2021-03-26 20:12:09 +0530

    Update subarray_with_given_sum.py

--- Arrays/subarray_with_given_sum.py
+++ Arrays/subarray_with_given_sum.py
@@ -1,3 +1,4 @@
+"""
 def subArraySum(A, n, x): 
     
     for i in range(n):
@@ -11,3 +12,28 @@ def subArraySum(A, n, x):
               
           j+=1
     return [-1]
+"""
+
+def subArrayExists(arr,n):
+        
+    s = set()
+
+    sum = 0
+    for i in range(n):
+        sum += arr[i]
+        print("sum",sum)
+        
+        if sum == 0 or sum in s:
+            return True
+            break
+        s.add(sum)
+        print("set",s)
+        
+    return False
+
+
+
+arr = [4,2,-3,1,6]
+ 
+print(subArrayExists(arr,5))
+ 
