c80131769c749d36b3e66465e531863103572fa2
RIYA JOSHI <riya.joshi@somaiya.edu>
2021-03-27 03:30:03 +0530

    geeks solution of max product sub array

---
+++ Arrays/max_product_sub_array.py
@@ -0,0 +1,33 @@
+class Solution:
+
+	# Function to find maximum
+	# product subarray
+	def maxProduct(self,arr, n):
+	    # Variables to store maximum and
+	    # minimum product till ith index.
+	    minVal = arr[0]
+	    maxVal = arr[0]
+
+	    maxProduct = arr[0]
+
+	    for i in range(1, n, 1):
+
+	        # When multiplied by -ve number,
+	        # maxVal becomes minVal
+	        # and minVal becomes maxVal.
+	        if (arr[i] < 0):
+	            temp = maxVal
+	            maxVal = minVal
+	            minVal = temp
+
+	        # maxVal and minVal stores the
+	        # product of subarray ending at arr[i].
+	        maxVal = max(arr[i], maxVal * arr[i])
+	        minVal = min(arr[i], minVal * arr[i])
+
+	        # Max Product of array.
+	        maxProduct = max(maxProduct, maxVal)
+
+	    # Return maximum product
+	    # found in array.
+	    return maxProduct
