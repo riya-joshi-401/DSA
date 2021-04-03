4a496c4c6f231bf520d5198f6ace900736bc0e07
RIYA JOSHI <riya.joshi@somaiya.edu>
2021-04-04 00:10:22 +0530

    Update prime.py

--- Mathematics/prime.py
+++ Mathematics/prime.py
@@ -1,3 +1,22 @@
+"""
+# A school method based Python3  program to check if a number is prime
+def isPrime(n):
+
+    # Corner case
+    if n <= 1:
+        return False
+
+    # Check from 2 to n-1
+    for i in range(2, n):
+        if n % i == 0:
+            return False;
+
+    return True
+"""
+
+#Efficient method
+# The factors from 2 to sqrt(n) have multiples from sqrt(n)+1 to n.
+
 class Solution:
     def isPrime (self, N):
         if(N == 1): # if N = 1 return 0
@@ -9,3 +28,5 @@ class Solution:
                 return 0
             i+=1
         return 1 # return 1 if N is not divisible by any other number.
+    
+    # For that 6k-+1 method check : https://en.wikipedia.org/wiki/Primality_test
