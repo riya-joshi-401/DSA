0d43a3ce5c3cb9bb9e9dc17923f9a16588bb380d
RIYA JOSHI <riya.joshi@somaiya.edu>
2021-05-26 12:57:59 +0530

    yet to understand :)

---
+++ Linked_List/reverse_sll.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverseList(self, head):
+        if head is None:
+            return None
+        
+        prev = None
+        curr = head
+        ahead = head.next
+        
+        while curr is not None:
+            ahead = curr.next       # Take the next node as ahead
+            curr.next = prev        # Link current node to its previous
+            prev = curr             # update prev as the current node
+            curr = ahead            # update curr
+        
+        return prev
