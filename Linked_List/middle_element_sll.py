9db22799bb941248e495caac174caac519c9aeca
RIYA JOSHI <riya.joshi@somaiya.edu>
2021-06-01 14:18:27 +0530

    two solutions :)

--- Linked_List/middle_element_sll.py
+++ Linked_List/middle_element_sll.py
@@ -9,3 +9,19 @@ def findMid(head):
     return li[len(li)//2]
     
 # in case of even length was asked to find the second middle element
+
+----------------------------------------------------------------------
+
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        cur=head
+        l=0
+        while(cur):
+            l+=1
+            cur=cur.next
+        l=l//2
+        while(l>0):
+            head=head.next
+            l-=1
+        return head
+
