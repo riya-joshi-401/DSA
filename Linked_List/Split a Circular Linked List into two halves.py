5c219c52f8c24f1fb0f13f8f043ad9f838c45524
RIYA JOSHI <riya.joshi@somaiya.edu>
2021-06-05 15:03:08 +0530

    Update Split a Circular Linked List into two halves.py

--- Linked_List/Split a Circular Linked List into two halves.py
+++ Linked_List/Split a Circular Linked List into two halves.py
@@ -12,11 +12,11 @@ class Solution:
                 break
         i=1
         mid=n//2 - 1 if n%2==0 else n//2
+        
         for i in range(n):
             
             if i==mid:
                 head2=temp.next
-                # temp.next=None
                 temp.next=head1
                 break
             else:
@@ -25,11 +25,11 @@ class Solution:
         ptr=head2
         
         for i in range(n-(n//2)):
+            
             if ptr.next==head1:
                 ptr.next=head2
                 break
             else:
                 ptr=ptr.next
             
-        
         return head1,head2
