ae62a3f66ccbbb07966d27e6c7ea87b5c1d74e68
RIYA JOSHI <riya.joshi@somaiya.edu>
2021-06-07 11:48:19 +0530

    Update pair.cpp

--- C++/STL/pair.cpp
+++ C++/STL/pair.cpp
@@ -13,23 +13,23 @@ void Pair() {
 
     // another way to initialize a pair is by using the make_pair() function. 
     new = make_pair(1, "hello");
-
-	pair<int, string> p = {1000, "hey"};
-	// prints 100 hey
-	cout << p.first << " " << p.second<<endl;
 	
-	pair<char, char> P;
+    pair<int, string> p = {1000, "hey"};
+    // prints 100 hey
+    cout << p.first << " " << p.second<<endl;
+	
+    pair<char, char> P;
     P.first = 'R';
     P.second = 'J';
     cout << P.first << " "<< P.second << endl;
 	
-	pair<int, pair<int, int>> q = {1, {2, 3}};
-	// prints 1 2 3
-	cout << q.first << " " << q.second.first << " " << q.second.second<<endl;
+    pair<int, pair<int, int>> q = {1, {2, 3}};
+    // prints 1 2 3
+    cout << q.first << " " << q.second.first << " " << q.second.second<<endl;
 
-	pair<int, int> arr[] = { {1, 2}, {3, 4}, {5, 6}};
-	// Prints 1
-	cout << arr[0].first<<endl;
+    pair<int, int> arr[] = { {1, 2}, {3, 4}, {5, 6}};
+    // Prints 1
+    cout << arr[0].first<<endl;
 	
 
 }
