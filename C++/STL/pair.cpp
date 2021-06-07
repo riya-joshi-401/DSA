bb093314aec597554a9289de64c3de1b6b0954d6
RIYA JOSHI <riya.joshi@somaiya.edu>
2021-06-07 11:47:07 +0530

    Update pair.cpp

--- C++/STL/pair.cpp
+++ C++/STL/pair.cpp
@@ -5,21 +5,23 @@ using namespace std;
 
 void Pair() {
     
-        // different ways to initialize
-    
-        pair  <int, int>p1;           //default
-        pair  <int, char>p2(1, 'a');  //initialized,  different data type
-        pair  <int, int>p3(1, 10);  //initialized,  same data type
-        pair  <int, int>p4(p3);    //copy of g3
+    // different ways to initialize
+    pair  <int, int>p1;           //default
+    pair  <int, char>p2(1, 'a');  //initialized,  different data type
+    pair  <int, int>p3(1, 10);  //initialized,  same data type
+    pair  <int, int>p4(p3);    //copy of g3
+
+    // another way to initialize a pair is by using the make_pair() function. 
+    new = make_pair(1, "hello");
 
 	pair<int, string> p = {1000, "hey"};
 	// prints 100 hey
 	cout << p.first << " " << p.second<<endl;
 	
 	pair<char, char> P;
-        P.first = 'R';
-        P.second = 'J';
-        cout << P.first << " "<< P.second << endl;
+    P.first = 'R';
+    P.second = 'J';
+    cout << P.first << " "<< P.second << endl;
 	
 	pair<int, pair<int, int>> q = {1, {2, 3}};
 	// prints 1 2 3
