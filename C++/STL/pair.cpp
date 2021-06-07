ddb8a1cdc1a28f0d310425c153fa6adb7a094196
RIYA JOSHI <riya.joshi@somaiya.edu>
2021-06-07 11:26:51 +0530

    Update pair.cpp

--- C++/STL/pair.cpp
+++ C++/STL/pair.cpp
@@ -5,20 +5,21 @@ using namespace std;
 
 void Pair() {
 
-	pair<int, string> p = {100, "hey"};
-
+	pair<int, string> p = {1000, "hey"};
 	// prints 100 hey
 	cout << p.first << " " << p.second<<endl;
-
-
+	
+	pair<char, char> P;
+    P.first = 'R';
+    P.second = 'J';
+    cout << P.first << " "<< P.second << endl;
+	
 	pair<int, pair<int, int>> q = {1, {2, 3}};
-
 	// prints 1 2 3
 	cout << q.first << " " << q.second.first << " " << q.second.second<<endl;
 
 
 	pair<int, int> arr[] = { {1, 2}, {3, 4}, {5, 6}};
-
 	// Prints 1
 	cout << arr[0].first<<endl;
 
