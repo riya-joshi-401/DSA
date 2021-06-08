46180451d65a1b8c55c1b4f0078b61f404887716
RIYA JOSHI <riya.joshi@somaiya.edu>
2021-06-08 15:18:11 +0530

    Update vector.cpp

--- C++/STL/vector.cpp
+++ C++/STL/vector.cpp
@@ -7,99 +7,111 @@ using namespace std;
 
 void Vector() {
 
-	// A empty vector
+	// Ways  for Vector initialization
+	
 	vector<int> v;  // {}
+	
+	vector<pair<int, int>>vec; // Vector having pair as its element
+	
+	vector<int> x(5, 100); // Vector of size 5 with everyone as 100 , {100, 100, 100, 100, 100}
 
-	v.push_back(1); // {1}
-	v.emplace_back(2); // {1, 2}
-
-	vector<pair<int, int>>vec;
-
-	v.push_back({1, 2});
-	v.emplace_back(1, 2);
-
-	// Vector of size 5 with
-	// everyone as 100
-	vector<int> v(5, 100); // {100, 100, 100, 100, 100}
-
-	// A vector of size 5
-	// initialized with 0
-	// might take garbage value,
-	// dependent on the vector
-	vector<int> v(5); // {0, 0, 0, 0, 0}
+	vector<int> y(5); // A vector of size 5 , initialized with 0 might take garbage value, dependent on the vector , {0, 0, 0, 0, 0}
 
 	vector<int> v1(5, 20); // {20, 20, 20, 20, 20}
-	vector<int> v2(v1); // {20, 20, 20, 20, 20}
+	
+	vector<int> v2(v1); // {20, 20, 20, 20, 20}{100, 100, 100, 100, 100}
+	
+	
+	/* Ways to insert data 
+	
+	1- push_back
+	2- emplace_back
+	*/
+
+	v.push_back(1); // {1}
+	vec.push_back({1, 2});
+	
+	v.emplace_back(2); // {1, 2}
+	vec.emplace_back(1, 2);
+	
+
+	
+
+    // Ways to print the vector and its elements
+    
+    // begin()-end() and cbegin()-cend(): prints in normal order , rbegin()-rend() and crbegin()-crend(): prints in reverse order
+
+    vector<int> g1;
+  
+    for (int i = 1; i <= 5; i++)
+        g1.push_back(i);
+  
+    cout << "Output of begin and end: ";
+    for (auto i = g1.begin(); i != g1.end(); ++i)
+        cout << *i << " ";
+  
+    cout << "\nOutput of cbegin and cend: ";
+    for (auto i = g1.cbegin(); i != g1.cend(); ++i)
+        cout << *i << " ";
+  
+    cout << "\nOutput of rbegin and rend: ";
+    for (auto ir = g1.rbegin(); ir != g1.rend(); ++ir)
+        cout << *ir << " ";
+  
+    cout << "\nOutput of crbegin and crend : ";
+    for (auto ir = g1.crbegin(); ir != g1.crend(); ++ir)
+        cout << *ir << " ";
+
+// 	// Take the vector to be {10, 20, 30, 40}
+// 	vector<int>::iterator it = v.begin();
+
+// 	it++;
+// 	cout << *(it) << " "; // prints 20
+
+
+// 	it = it + 2;
+// 	cout << *(it) << " "; // prints 30
+
+// 	vector<int>::iterator it = v.end();
+
+// 	vector<int>::iterator it = v.rend();
+
+// 	vector<int>::iterator it = v.rbegin();
 
 
 
-	// Take the vector to be {10, 20, 30, 40}
-	vector<int>::iterator it = v.begin();
+// 	cout << v[0] << " " << v.at(0);
 
-	it++;
-	cout << *(it) << " "; // prints 20
+// 	cout << v.back() << " ";
 
+// 	// {10, 20, 12, 23}
+// 	v.erase(v.begin()+1);
 
-	it = it + 2;
-	cout << *(it) << " "; // prints 30
+// 	// {10, 20, 12, 23, 35}
+// 	v.erase(v.begin() + 2, v.begin() + 4); // // {10, 20, 35} [start, end)
 
-	vector<int>::iterator it = v.end();
+// 	// Insert function
 
-	vector<int>::iterator it = v.rend();
+// 	vector<int>v(2, 100); // {100, 100}
+// 	v.insert(v.begin(), 300); // {300, 100, 100};
+// 	v.insert(v.begin() + 1, 2, 10); // {300, 10, 10, 100, 100}
 
-	vector<int>::iterator it = v.rbegin();
+// 	vector<int> copy(2, 50); // {50, 50}
+// 	v.insert(v.begin(), copy.begin(), copy.end()); // {50, 50, 300, 10, 10, 100, 100}
 
+// 	// {10, 20}
+// 	cout << v.size(); // 2
 
+// 	//{10, 20}
+// 	v.pop_back(); // {10}
 
-	cout << v[0] << " " << v.at(0);
+// 	// v1 -> {10, 20}
+// 	// v2 -> {30, 40}
+// 	v1.swap(v2); // v1 -> {30, 40} , v2 -> {10, 20}
 
-	cout << v.back() << " ";
+// 	v.clear(); // erases the entire vector
 
-
-	// Ways to print the vector
-
-	for (vector<int>::iterator it = v.begin(); it != v.end(); it++) {
-		cout << *(it) << " ";
-	}
-
-
-	for (auto it = v.begin(); it != v.end(); it++) {
-		cout << *(it) << " ";
-	}
-
-	for (auto it : v) {
-		cout << it << " ";
-	}
-
-	// {10, 20, 12, 23}
-	v.erase(v.begin()+1);
-
-	// {10, 20, 12, 23, 35}
-	v.erase(v.begin() + 2, v.begin() + 4); // // {10, 20, 35} [start, end)
-
-
-	// Insert function
-
-	vector<int>v(2, 100); // {100, 100}
-	v.insert(v.begin(), 300); // {300, 100, 100};
-	v.insert(v.begin() + 1, 2, 10); // {300, 10, 10, 100, 100}
-
-	vector<int> copy(2, 50); // {50, 50}
-	v.insert(v.begin(), copy.begin(), copy.end()); // {50, 50, 300, 10, 10, 100, 100}
-
-	// {10, 20}
-	cout << v.size(); // 2
-
-	//{10, 20}
-	v.pop_back(); // {10}
-
-	// v1 -> {10, 20}
-	// v2 -> {30, 40}
-	v1.swap(v2); // v1 -> {30, 40} , v2 -> {10, 20}
-
-	v.clear(); // erases the entire vector
-
-	cout << v.empty();
+// 	cout << v.empty();
 
 }
 
