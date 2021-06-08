7acabaada8d361799d2aafe85eb034b29cbde339
RIYA JOSHI <riya.joshi@somaiya.edu>
2021-06-08 17:25:52 +0530

    Create list.cpp

---
+++ C++/STL/list.cpp
@@ -0,0 +1,4 @@
+//almost same as vector 
+
+// https://www.geeksforgeeks.org/list-cpp-stl/#:~:text=List%20in%20C%2B%2B%20Standard%20Template%20Library%20%28STL%29%20-,the%20first%20element%20of%20the%20list.%20More%20items
+
