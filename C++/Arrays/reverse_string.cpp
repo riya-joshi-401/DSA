6e134a509cdbff628090428d78b4b9a3918e4fc6
RIYA JOSHI <riya.joshi@somaiya.edu>
2021-06-19 13:11:30 +0530

    Update reverse_string.cpp

--- C++/Arrays/reverse_string.cpp
+++ C++/Arrays/reverse_string.cpp
@@ -1,3 +1,7 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
 string reverseWord(string str){
   
   reverse(str.begin(), str.end());  // in-built function for reversing string in c++
