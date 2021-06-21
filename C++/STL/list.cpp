460bb69843d02170145c91be256c23c29f18b266
RIYA JOSHI <riya.joshi@somaiya.edu>
2021-06-21 15:27:46 +0530

    very imp link containing important list stl functions

--- C++/STL/list.cpp
+++ C++/STL/list.cpp
@@ -1,4 +1,6 @@
 //almost same as vector 
 
+// VERY IMP : https://www.includehelp.com/stl/list-functions-in-cpp-stl.aspx
+
 // https://www.geeksforgeeks.org/list-cpp-stl/#:~:text=List%20in%20C%2B%2B%20Standard%20Template%20Library%20%28STL%29%20-,the%20first%20element%20of%20the%20list.%20More%20items
 
