2878a41b4b3596f73bfefd633a6aac0eaf183be9
RIYA JOSHI <riya.joshi@somaiya.edu>
2021-06-23 00:54:31 +0530

    easy peasy

---
+++ C++/Arrays/median of an array.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+	public:
+		int find_median(vector<int> v)
+		{
+		    int n=v.size();
+		    sort(v.begin(),v.end());
+		    if(n%2==0){
+		        return (v[n/2]+v[n/2-1])/2;
+		    }
+		    else{
+		        return v[n/2];
+		    }
+		}
+};
