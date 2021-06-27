90344c8364c2048d574746d6dd17c4aaf3871277
RIYA JOSHI <riya.joshi@somaiya.edu>
2021-06-28 00:11:36 +0530

    hmmmm

--- Arrays/subarray exists with sum equal to 0 or not.py
+++ Arrays/subarray exists with sum equal to 0 or not.py
@@ -1,18 +1,4 @@
-"""
-def subArraySum(A, n, x): 
-    
-    for i in range(n):
-      temp=0
-      j=i
-      
-      while (temp <x) and (j<n):
-          temp=temp+A[j]
-          if temp==x:
-              return [i+1,j+1]
-              
-          j+=1
-    return [-1]
-"""
+
 
 def subArrayExists(arr,n):
         
