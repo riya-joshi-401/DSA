ba9a671d8f1ef73604a3453030ccb74ad99f8640
RIYA JOSHI <riya.joshi@somaiya.edu>
2021-06-28 00:12:22 +0530

    Update subarray exists with sum equal to 0 or not.py

--- Arrays/subarray exists with sum equal to 0 or not.py
+++ Arrays/subarray exists with sum equal to 0 or not.py
@@ -1,28 +1,16 @@
+class Solution:
 
-
-def subArrayExists(arr,n):
+    def subArrayExists(self,arr,n):
         
-    s = set()
+        s = set()
 
-    sum = 0
-    for i in range(n):
-        sum += arr[i]
-        print("sum",sum)
-        
-        #if prefix sum is 0 or if it is already present in set then it is
-    		#repeated which means there is a subarray whose summation was 0,
-    		#so we return true.
-        if sum == 0 or sum in s:
-            return True
-            break
-        s.add(sum)
-        print("set",s)
-        
-    return False
-
-
-
-arr = [4,2,-3,1,6]
- 
-print(subArrayExists(arr,5))
- 
+        sum = 0
+        for i in range(n):
+            sum += arr[i]
+            
+            if sum == 0 or sum in s:
+                return True
+                break
+            s.add(sum)
+            
+        return False
