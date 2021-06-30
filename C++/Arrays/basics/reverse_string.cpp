de2de29376146611ee5c9e96aee452a4cba8ee0f
RIYA JOSHI <riya.joshi@somaiya.edu>
2021-07-01 01:19:09 +0530

    Create reverse_string.cpp

---
+++ C++/Arrays/basics/reverse_string.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+string reverseWord(string str){
+  
+  reverse(str.begin(), str.end());  // in-built function for reversing string in c++
+  
+  return str;
+}
