fa8a5f3e366537e080d1b0defc283f1ba4934616
RIYA JOSHI <riya.joshi@somaiya.edu>
2021-07-02 00:38:37 +0530

    Create encapsulation.cpp

---
+++ OOPS/encapsulation.cpp
@@ -0,0 +1,32 @@
+// Encapsulation is defined as wrapping up of data and information under a single unit
+
+/*
+
+Consider a real life example of encapsulation, in a company there are different sections like the accounts
+section, finance section, sales section etc. The finance section handles all the financial transactions and 
+keep records of all the data related to finance. Similarly the sales section handles all the sales related 
+activities and keep records of all the sales. Now there may arise a situation when for some reason an official 
+from finance section needs all the data about sales in a particular month. In this case, he is not allowed to 
+directly access the data of sales section. He will first have to contact some other officer in the sales section 
+and then request him to give the particular data. This is what encapsulation is. Here the data of sales section and 
+the employees that can manipulate them are wrapped under a single name “sales section”.
+
+*/
+
+// Encapsulation also lead to data abstraction or hiding. As using encapsulation also hides the data.
+
+// exaample: like if someone isdriving a car so he/she might not know the car's actual mechanical part and inner structure he only knows break, 
+// accelator, and steering.So, here basically the information about the car is hidden from the driver for simplification of things.
+
+// A class has two things: data memebers and member functions
+// Access modifiers: public,private, protected
+// 1- Public: objects can access these data members and fucntions outside the class, inside the class and inside the derived/inherited class
+// 2- Private: objects cannot access the data members and functions outside the class or from a derived/inherited class. They can be only accessed inside the class.
+// 3- Protected: objects cannot access the data members and functions outside the class , They can accessed inside the classs and derived/inherited class.
+
+// Advantages:
+// good practice, increases security of our data
+
+// code: https://www.geeksforgeeks.org/encapsulation-in-c/
+
+
